da319592b60a7e61bfd9b1ee071b47ff9f5601fb
sbargavy786-hub <sbargavy.786@gmail.com>
2026-06-04 21:55:34 +0530

    add kinetic-energy.py to calculate kinetic energy based on mass and velocity

---
+++ kinetic-energy.py
@@ -0,0 +1,4 @@
+mass= float(input("Enter the mass of the object in kilograms: "))
+velocity= float(input("Enter the velocity of the object in meters per second: "))
+kinetic_energy = 0.5 * mass * velocity ** 2
+print("The kinetic energy of the object is:", kinetic_energy, "joules")
