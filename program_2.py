389eee9c8127dde0cf1937edc3d1175040f9fef8
sbargavy786-hub <sbargavy.786@gmail.com>
2026-06-05 22:29:06 +0530

    add program_2.py for number comparison and voltage validation

---
+++ program_2.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+if x > y:
+    print(f"{x} is greater than {y}")
+elif x < y:
+    print(f"{y} is greater than {x}")
+else:
+    print("Both numbers are equal")
+
+voltage = 500
+volt = int(input("Enter the voltage:"))
+if voltage == volt:
+    print("Voltage is correct")
+else:
+    print("Voltage is incorrect")
