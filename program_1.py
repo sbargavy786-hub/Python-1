a17ca8fced8e9d83ac167295bd0d65df3ab6cd90
sbargavy786-hub <sbargavy.786@gmail.com>
2026-06-05 22:17:01 +0530

    add program_1.py to calculate total and average scores for Maths, Physics, and Chemistry

---
+++ program_1.py
@@ -0,0 +1,6 @@
+maths = int(input("Maths score:"))
+physics = int(input("Physics score:"))
+chemistry = int(input("Chemistry score:"))
+total = maths + physics + chemistry
+average = total / 3
+print(f"Total score: {total},60 Average score: {average}")
